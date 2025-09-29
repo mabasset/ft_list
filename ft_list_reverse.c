c77bba8305b017516b196ea6a4ced639361ca627
mabasset <matte.bassetto01@gmail.com>
2025-09-29 10:06:28 +0000

    ft_list_reverse fix

--- ft_list_reverse.c
+++ ft_list_reverse.c
@@ -4,6 +4,7 @@ void    ft_list_reverse(t_list **begin_list) {
     t_list  *new_list;
     t_list  *tmp;
 
+    new_list = NULL;
     while (*begin_list != NULL) {
         ft_list_push_front(&new_list, (*begin_list)->data);
         tmp = (*begin_list)->next;
